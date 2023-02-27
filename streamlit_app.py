dc035b9b882b2c0254a62999334840d7906a8c0b
FlorianWartel <90322024+FlorianWartel@users.noreply.github.com>
2023-02-27 18:17:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -19,19 +20,25 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+# Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 streamlit.write('The user entered ', fruit_choice)
 
 #New section to display api response 
 streamlit.header("Fruityvice Fruit Advice!")
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# create df
-streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -42,7 +49,7 @@ streamlit.dataframe(my_data_rows)
 
 fruit_add = streamlit.text_input('What fruit would you like to add ?','Kiwi')
 #if fruit_add in my_data_rows:
-my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.fruit_load_list VALUES ('" + fruit_add + "')")
+#my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.fruit_load_list VALUES ('" + fruit_add + "')")
 streamlit.write('Thanks for adding ', fruit_add)
 
 
